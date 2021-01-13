1cb2b702e4be3709432629b39836890aeadc7249
DanilAndreev <33941552+DanilAndreev@users.noreply.github.com>
2021-01-13 15:26:01 +0200

    [copyright] Added.

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) 2021. This code created and belongs to Atlas render manager project.
+ * Owner and project architect: Danil Andreev | danssg08@gmail.com |  https://github.com/DanilAndreev
+ * Project: atlas-website
+ * File last modified: 10/26/85, 10:15 AM
+ * All rights reserved.
+ */
+
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
