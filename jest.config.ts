2c7e989ed6b8b1c68816eefc9dcecaacc98081af
DanilAndreev <33941552+DanilAndreev@users.noreply.github.com>
2021-01-13 16:43:33 +0200

    [package] Added some new options. [Jest] Added jest testing.

---
+++ jest.config.ts
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2021. This code created and belongs to Atlas render manager project.
+ * Owner and project architect: Danil Andreev | danssg08@gmail.com |  https://github.com/DanilAndreev
+ * Project: atlas-website
+ * File last modified: 1/13/21, 4:39 PM
+ * All rights reserved.
+ */
+
+import type {Config} from '@jest/types';
+
+const config: Config.InitialOptions = {
+    verbose: true,
+};
+export default config;
---
+++ tests/Temp.test.ts
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2021. This code created and belongs to Atlas render manager project.
+ * Owner and project architect: Danil Andreev | danssg08@gmail.com |  https://github.com/DanilAndreev
+ * Project: atlas-website
+ * File last modified: 1/13/21, 4:40 PM
+ * All rights reserved.
+ */
+
+describe("Test testing suit", () => {
+   test("Test", () => {
+       expect(1).toBe(1);
+   })
+});
